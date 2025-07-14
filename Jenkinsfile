ce0d6b860901abde5bd18109bb0eefaf6a8c1ac8
Prajwal Rawate <your.prajwalrawate2@gmail.com>
2025-07-14 15:21:05 +0530

    first commit

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,26 +1,68 @@
 
-pipelines {
-    agent :any
-    environment{
-      AWS_DEFAULT_REGION ="eu-north-1"
+// pipeline{
+//     agent :any
+//     environment{
+//       AWS_DEFAULT_REGION ="eu-north-1"
+//     }
+//   stages{
+//     stage("git command"){
+//   steps {
+//                 git branch: 'main', url: 'https://github.com/Prajwal299/boto3-practise.git'
+//             }
+//     }
+//     stage('Install Dependencies') {
+//             steps {
+//                bat "pip install boto3"
+//             }
+//         }
+//     stage("launche ec2 with docker"){
+//         steps{
+//             withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS-Creds']]){
+//                 bat "ec2_creator.py"
+//             } 
+//         }
+//     }
+//   }
+// }
+
+
+pipeline {
+    agent any
+
+    environment {
+        AWS_DEFAULT_REGION = "eu-north-1"
     }
-  stages{
-    stage("git command"){
-  steps {
-                git branch: 'main', credentialsId: 'https://github.com/Prajwal299/boto3-practise.git'
-            }
-    }
-    stage('Install Dependencies') {
+
+    stages {
+
+        stage('Clone GitHub Repo') {
             steps {
-               bat "pip install boto3"
+                git branch: 'main', credentialsId: 'your-git-credentials-id', url: 'https://github.com/Prajwal299/boto3-practise.git'
             }
         }
-    stage("launche ec2 with docker"){
-        steps{
-            withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS-Creds']]){
-                bat "ec2_creator.py"
-            } 
+
+        stage('Install boto3') {
+            steps {
+                sh 'pip install boto3'
+            }
+        }
+
+        stage('Launch EC2 with Docker') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS-Creds']]) {
+                    sh 'python3 ec2_creator.py'
+                }
+            }
+        }
+
+    }
+
+    post {
+        failure {
+            echo 'Build failed!'
+        }
+        success {
+            echo 'EC2 instance launched!'
         }
     }
-  }
 }
--- ec2_creator.py
+++ ec2_creator.py
@@ -1,40 +1,107 @@
 
 
+# import boto3
+
+# session = boto3.Session(region_name='eu-north-1')  
+# ec2_console = session.client('ec2')
+
+# instance = ec2_console.run_instances(
+#     ImageId='ami-042b4708b1d05f512',
+#     MinCount=1,
+#     MaxCount=1,
+#     InstanceType='t3.micro',
+#     TagSpecifications=[
+#         {
+#             'ResourceType': 'instance',
+#             'Tags': [
+#                 {
+#                     'Key': 'Name',
+#                     'Value': 'Boto3-deployment-1'
+#                 }
+#             ]
+#         }
+#     ],
+#     SecurityGroupIds=['sg-0cd0055363c2a2d75'],
+#     userData="""
+#     #!/bin/bash
+#     sudo apt-get update
+#     sudo apt-get upgrade -y
+#     sudo apt install docker.io -y
+
+#     sudo systemctl start docker
+#     sudo systemctl enable docker
+
+#     sudo usermod -aG docker ubuntu
+#      """
+    
+# )
+
+# instance_id = instance['Instances'][0]['InstanceId']
+# print("Launched instance:", instance_id)
+
+
+
 import boto3
+import time
 
-session = boto3.Session(region_name='eu-north-1')  
-ec2_console = session.client('ec2')
+session = boto3.Session(region_name='eu-north-1')
+ec2 = session.client('ec2')
 
-instance = ec2_console.run_instances(
-    ImageId='ami-042b4708b1d05f512',
+# Update with your actual values
+AMI_ID = 'ami-042b4708b1d05f512'
+INSTANCE_TYPE = 't3.micro'
+SECURITY_GROUP_ID = 'sg-0cd0055363c2a2d75'
+KEY_NAME = 'your-key-name'  # Required if you want to SSH in manually
+
+# EC2 UserData script
+user_data_script = """#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y docker.io git
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+
+# Clone your Flask app project from GitHub
+cd /home/ubuntu
+git clone https://github.com/Prajwal299/boto3-practise.git
+
+cd boto3-practise/flask-app-3
+
+# Build Docker image
+sudo docker build -t flask-app-3 .
+
+# Run container
+sudo docker run -d -p 5000:5000 --name flask-app-3 flask-app-3
+"""
+
+# Launch EC2 instance
+response = ec2.run_instances(
+    ImageId=AMI_ID,
     MinCount=1,
     MaxCount=1,
-    InstanceType='t3.micro',
+    InstanceType=INSTANCE_TYPE,
+    KeyName=KEY_NAME,
+    SecurityGroupIds=[SECURITY_GROUP_ID],
     TagSpecifications=[
         {
             'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': 'Boto3-deployment-1'
-                }
-            ]
+            'Tags': [{'Key': 'Name', 'Value': 'Boto3-deployment-1'}]
         }
     ],
-    SecurityGroupIds=['sg-0cd0055363c2a2d75'],
-    userData="""
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt install docker.io -y
-
-    sudo systemctl start docker
-    sudo systemctl enable docker
-
-    sudo usermod -aG docker ubuntu
-     """
-    
+    UserData=user_data_script
 )
 
-instance_id = instance['Instances'][0]['InstanceId']
+instance_id = response['Instances'][0]['InstanceId']
 print("Launched instance:", instance_id)
+
+# Wait until the instance is running
+ec2_resource = session.resource('ec2')
+instance = ec2_resource.Instance(instance_id)
+
+print("Waiting for instance to be in 'running' state...")
+instance.wait_until_running()
+
+# Refresh instance data
+instance.load()
+print("Public IP:", instance.public_ip_address)
